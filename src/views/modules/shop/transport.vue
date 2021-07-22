6bb68cd885b2e07191d6a9be5bb97fa26611a929
cl <mumu781536077@163.com>
2021-07-22 15:09:41 +0800

    运费模板修改

--- src/views/modules/shop/transport.vue
+++ src/views/modules/shop/transport.vue
@@ -105,7 +105,7 @@ export default {
     deleteHandle (id) {
       var ids = id ? [id] : this.dataListSelections.map(item => { return item.transportId })
       this.$confirm(
-        `确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`,
+        `确定进行[${id ? '删除' : '批量删除'}]操作?`,
         '提示',
         {
           confirmButtonText: '确定',
